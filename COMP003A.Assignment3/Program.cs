be981f9a2f507c7d3368a9a77061e874736e38c0
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-01-30 14:15:58 -0800

    Added Behavoir 1 that covers the ages 18-25

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=== Academic & Personal Readiness Evaluator ===");
+            Console.WriteLine();
+
+            // Behavior 1
+            int currentYear = DateTime.Now.Year;
+            Console.WriteLine("Current Year: " + currentYear);
+
+            Console.WriteLine("Enter your birth year: ");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            int age = currentYear - birthYear;
+            Console.WriteLine();
+            Console.WriteLine("Calculated Age: " + age);
+
+            string readinessCategory = "";
+            if (age < 18)
+            {
+                readinessCategory = "Minor";
+            }
+            else if (age <= 25)
+            {
+                readinessCategory = "Adult";
+            }
+
+            Console.WriteLine("Readiness Category: " + readinessCategory);
+            Console.WriteLine();
+
         }
     }
 }
